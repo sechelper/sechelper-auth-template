b0e0e18dfe871ce216ff1ef5548675898d42d332
cookun <cookun@sechelper.com>
2026-09-17 17:31:08 +0800

    fix: restore framework configuration page rendering

--- web/admin/src/modules/deployment/DeploymentGuidePage.jsx
+++ web/admin/src/modules/deployment/DeploymentGuidePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PageHeader } from "../../app/components.jsx";
 import { configurationApi } from "../configuration/api.js";
 
